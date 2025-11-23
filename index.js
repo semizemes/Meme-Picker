70975c3cf9d0e6ebaa6b773f14a997f67ea4dcfa
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2025-11-23 21:37:48 +0500

    refactor: improve formatting and consistency in getEmotionsArray and renderEmotionsRadios functions

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-import { catsData } from "./data.js"
+import { catsData } from "./data.js";
 
-const emotionRadios = document.getElementById("emotion-radios")
+const emotionRadios = document.getElementById("emotion-radios");
 
-function getEmotionsArray(cats){
-    const emotionsArray = []
+function getEmotionsArray(cats) {
+  const emotionsArray = [];
 
-    for (const cat of cats) {
-        for (const emotion of cat.emotionTags) {
-            emotionsArray.push(emotion);
-        }
+  for (const cat of cats) {
+    for (const emotion of cat.emotionTags) {
+      emotionsArray.push(emotion);
     }
-    
-    return emotionsArray;
+  }
+
+  return emotionsArray;
 }
 
-function renderEmotionsRadios(cats){
-    const emotions = getEmotionsArray(cats)
+function renderEmotionsRadios(cats) {
+  const emotions = getEmotionsArray(cats);
 
-    let radioItems = '';
+  let radioItems = "";
 
-    for (const emotion of emotions) {
-        radioItems += `<p>${emotion}</p>`
-    }
+  for (const emotion of emotions) {
+    radioItems += `
+            <div class="radio">
+                <label for="${emotion}">${emotion}</label>
+                <input 
+                    type="radio"
+                    name='emotions'
+                    id="${emotion}"
+                    value="${emotion}"
+                >
+            </div>
+            `;
+  }
 
-    emotionRadios.innerHTML = radioItems;
+  emotionRadios.innerHTML = radioItems;
 }
 
 renderEmotionsRadios(catsData);
