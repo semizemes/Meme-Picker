0e9c5abe7d811a110723bac69f96f4448d6e46eb
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2025-11-24 19:28:01 +0500

    refactor: add highlightCheckedOption function to manage selected emotion state

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ import { catsData } from "./data.js";
 
 const emotionRadios = document.getElementById("emotion-radios");
 
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+function highlightCheckedOption(e) {
+  console.log(e);
+  document.getElementById(e.target.id).classList.add("highlight");
+}
+
+// making emotions array and sorted duplicate elements
 function getEmotionsArray(cats) {
   const emotionsArray = [];
 
@@ -16,6 +24,8 @@ function getEmotionsArray(cats) {
   return emotionsArray;
 }
 
+
+// get emotions array and render in page
 function renderEmotionsRadios(cats) {
   const emotions = getEmotionsArray(cats);
 
